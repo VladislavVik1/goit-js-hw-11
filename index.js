16e2c7e189b5e2adc06989c4782502cf5c0c5c61
VladislavVik1 <Dakraman1232@ukr.net>
2025-03-17 16:31:04 +0000

    Deploying to gh-pages from @ VladislavVik1/goit-js-hw-11@b1c3c1864a93d9f167c46b1dd8901af4022f5940 🚀

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&c(n)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function c(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const i=document.getElementById("search-form");i.addEventListener("submit",async s=>{s.preventDefault();const t=s.target.elements["search-text"].value.trim();if(!t){console.warn("Порожній запит");return}try{const o=await fetchImages(t);renderImages(o)}catch(o){console.error("Помилка отримання зображень:",o)}});
+import{a as d,S as u,i as n}from"./assets/vendor-5l-LjSpL.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function a(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=a(e);fetch(e.href,t)}})();const p="https://pixabay.com/api/",m="49353739-de52a4a64f2d40a4c6a535a2b";function f(o){return d.get(p,{params:{key:m,q:o,image_type:"photo",orientation:"horizontal",safesearch:!0}})}const l=document.querySelector(".gallery");function y(o){l.innerHTML="";const s=o.map(r=>`
+        <li class="gallery-item">
+            <a class="gallery-link" href="${r.largeImageURL}">
+                <img src="${r.webformatURL}" alt="${r.tags}" width="360" height="240" />
+            </a>
+            <div class="description">
+                <p class="gallery-item-text">Likes</p>
+                <span>${r.likes}</span>
+            </div>
+            <div class="description">
+                <p class="gallery-item-text">Views</p>
+                <span>${r.views}</span>
+            </div>
+            <div class="description">
+                <p class="gallery-item-text">Comments</p>
+                <span>${r.comments}</span>
+            </div>
+            <div class="description">
+                <p class="gallery-item-text">Downloads</p>
+                <span>${r.downloads}</span>
+            </div>
+        </li>`).join("");l.innerHTML=s,new u(".gallery a",{captionsData:"alt",captionDelay:250}).refresh()}const h=document.querySelector(".form"),c=document.querySelector(".loader");h.addEventListener("submit",g);function g(o){o.preventDefault();const s=document.querySelector('input[name="search-text"]').value.trim();if(s===""){n.show({title:"Error",message:"Please enter a search query!"});return}c.classList.remove("visually-hidden"),f(s).then(a=>{a.data.hits.length===0?n.show({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}):(console.log(a.data),y(a.data.hits))}).catch(a=>{console.error(a),n.show({title:"Error",message:"Sorry, there was an error processing your request. Please try again!"})}).finally(()=>c.classList.add("visually-hidden"))}
 //# sourceMappingURL=index.js.map
